bd98bf57f71d867f948b37b19d51ff1165fd4e7f
samuelsantanaoficial <psicopatadamusica@gmail.com>
2026-01-04 14:13:24 -0300

    fix: bootstrap offline pwa

--- sw.js
+++ sw.js
@@ -5,7 +5,10 @@ const ASSETS = [
 	'./style.css',
 	'./main.js',
 	'./manifest.json',
-	'./maskable_icon_x512.png'
+	'./maskable_icon_x512.png',
+	'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+	'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css',
+	'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js',
 ];
 
 self.addEventListener('install', (e) => {
