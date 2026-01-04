59a9e88fe022038a34958426bc3d9fc084827855
samuelsantanaoficial <psicopatadamusica@gmail.com>
2026-01-04 14:12:11 -0300

    fix: update pwa

--- sw.js
+++ sw.js
@@ -8,35 +8,37 @@ const ASSETS = [
 	'./maskable_icon_x512.png'
 ];
 
-// 1. Instalação: Salva os arquivos no cache
 self.addEventListener('install', (e) => {
 	e.waitUntil(
-		caches.open(CACHE_NAME).then((cache) => {
-			return cache.addAll(ASSETS);
-		})
+		caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
 	);
+	self.skipWaiting();
 });
 
-// 2. Ativação: Limpa caches antigos se mudar a versão
 self.addEventListener('activate', (e) => {
 	e.waitUntil(
 		caches.keys().then((keyList) => {
 			return Promise.all(
 				keyList.map((key) => {
-					if (key !== CACHE_NAME) {
-						return caches.delete(key);
-					}
+					if (key !== CACHE_NAME) return caches.delete(key);
 				})
 			);
 		})
 	);
+	self.clients.claim();
 });
 
-// 3. Interceptação: Serve o arquivo do cache se estiver offline
 self.addEventListener('fetch', (e) => {
 	e.respondWith(
-		caches.match(e.request).then((response) => {
-			return response || fetch(e.request);
+		caches.match(e.request).then((cachedResponse) => {
+			const fetchPromise = fetch(e.request).then((networkResponse) => {
+				caches.open(CACHE_NAME).then((cache) => {
+					cache.put(e.request, networkResponse.clone());
+				});
+				return networkResponse;
+			});
+
+			return cachedResponse || fetchPromise;
 		})
 	);
 });
